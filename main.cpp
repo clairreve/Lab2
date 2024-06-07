811b38c62be380675bab9dd9444328167042b8cf
yoochan <clairreve629@snu.ac.kr>
2024-06-07 08:04:08 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -7,13 +7,21 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    if(cnt>=M)
+    {
+        for(auto it:vec) cout<<it<<" ";
+        cout<<endl;
+        return;
+    }
+    for(int i=1;i<=N;i++)
+    {
+        if(vec.empty()||vec.back()<i)
+        {
+            vec.push_back(i);
+            func(cnt+1);
+            vec.erase(vec.end()-1);
+        }
+    }
 }
 
 int main() {
